ebe0df2da0b432cf73b12369900e26a416793e23
Игорь Половко <bujhm2607@yandex.ru>
2024-09-13 23:28:30 +0700

    add auth

---
+++ user.py
@@ -0,0 +1,30 @@
+# Import smtplib for the actual sending function
+import smtplib
+import os
+from email.message import EmailMessage
+from email.utils import make_msgid
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+code = 2607
+template = f'''<div style="background-color: lightblue; padding: 20px;">
+    <div style="background-color: white; padding: 10px;">
+       Введите проверочный код {code}
+    </div>
+</div>'''
+asparagus_cid = make_msgid()
+textfile = 'README.md'
+msg = EmailMessage()
+# with open(textfile) as fp:
+#     msg.set_content(fp.read())
+msg.set_content(template.format(asparagus_cid=asparagus_cid), subtype='html')
+msg['Subject'] = f'Проверочный код'
+msg['From'] = os.getenv("me")
+msg['To'] = os.getenv('you')
+s = smtplib.SMTP('smtp.yandex.ru')
+s.starttls()
+s.login(os.getenv("me"), os.getenv("PASS"))
+s.send_message(msg)
+s.quit()
